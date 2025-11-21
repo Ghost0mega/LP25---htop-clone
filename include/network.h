a45da2308d108aca6729b336f9b2a4928f4f3c8e
Lofane <sguyenot009@gmail.com>
2025-11-21 15:42:05 +0100

    Structs : init

--- include/network.h
+++ include/network.h
@@ -1,6 +1,20 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+typedef enum connection_type {
+    CONN_SSH,
+    CONN_TELNET
+} connection_type_t;
+
+typedef struct remote_config {
+    char name[64];
+    char address[128];
+    int port;
+    char username[64];
+    char password[64];
+    connection_type_t type;
+} remote_config_t;
+
 /* Network API (stub) */
 
 int network_init(void);
