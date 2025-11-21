d811e4a5af3c8d14c86165cf170eaef8c86b7f40
Lofane <165309408+Ghost0mega@users.noreply.github.com>
2025-11-21 15:42:05 +0100

    Structs : init

--- include/process.h
+++ include/process.h
@@ -1,6 +1,15 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+typedef struct process_info {
+    int pid;
+    char name[256];
+    char user[64];
+    double cpu_usage;
+    double mem_usage;
+    long uptime;
+} process_info_t;
+
 /* Process handling API (stub) */
 
 int process_init(void);
