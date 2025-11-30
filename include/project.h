701181e50071088d6da8b525c11646d416c7c052
AilysGeister <samantha.guivret@gmail.com>
2025-11-30 18:51:18 +0100

    gestion des arguments

--- include/project.h
+++ include/project.h
@@ -5,6 +5,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "config.h"
 #include "utils.h"
@@ -13,6 +16,7 @@
 #include "machine.h"
 #include "network.h"
 #include "ui.h"
+#include "parameters.h"
 
 #endif /* PROJECT_H */
 
